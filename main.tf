d05182bef77b9f57b18a844eab940ebe92945c97
faraday23 <41235618+faraday23@users.noreply.github.com>
2020-11-20 10:45:38 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,46 +1,85 @@
-# SQL Server Database within a SQL Server Server
-resource "azurerm_mssql_database" "db" {
-  name               = var.name
-  server_id          = var.server_id
-  collation          = var.collation
-  license_type       = var.license_type
-  sku_name           = var.sku_name
-  max_size_gb        = var.max_size_gb
-  zone_redundant     = var.zone_redundant
-  read_scale         = var.read_scale
-  read_replica_count = var.read_replica_count
+locals {
+  region_list    = ["eastus", "westus", "eastus2", "centralus", "westus2", "westcentralus", "australiaeast", "australiasoutheast", "southeastasia", "eastasia", "northeurope", "westeurope", "uksouth", "ukwest", "southafricanorth", "southafricawest", "centralindia", "southindia", "japaneast", "japanwest", "koreacentral", "koreasouth", "canadacentral", "canadaeast", "francecentral", "francesouth", "germanywestcentral", "germanynorth", "norwayeast", "norwaywest", "switzerlandnorth", "switzerlandwest", "uaenorth", "uaecentral", "southcentralus", "centraluseuap", "brazilsoutheast", "brazilsouth"]
+  paired_regions = { for pair in chunklist(concat(local.region_list, reverse(local.region_list)), 2) : pair[0] => pair[1] }
 
-  auto_pause_delay_in_minutes = var.sku_name == "GP_S_Gen5_1" ? var.auto_pause_delay_in_minutes : 0
-  min_capacity                = var.sku_name == "GP_S_Gen5_1" ? var.min_capacity : 0
+  secondary_region = local.paired_regions[lower(replace(var.location, " ", ""))]
+  replica_enabled  = var.create_mode == "Replica"
 }
 
-# Diagnostic setting
-module "ds_mssql_server" {
-  source                         = "github.com/faraday23/terraform-azurerm-monitor-diagnostic-setting.git"
-  storage_account                = var.storage_endpoint
-  sa_resource_group              = var.storage_account_resource_group
-  target_resource_id             = azurerm_mssql_database.db.id
-  target_resource_name           = "${var.name}-mssql${var.server_id}"
-  ds_allmetrics_rentention_days  = var.metric
+resource "azurerm_storage_account" "sa" {
+  name                     = format("%s%ssa%02s", var.names.product_name, var.names.environment, 1)
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = local.replica_enabled ? "GZRS" : "LRS"
+}
 
-  ds_log_api_endpoints = {
-  "AutomaticTuning"             = var.automatic_tuning,
-  "Blocks"                      = var.blocks,
-  "DatabaseWaitStatistics"      = var.database_wait_statistics,
-  "Deadlocks"                   = var.deadlocks,
-  "Errors"                      = var.error_log, 
-  "Timeouts"                    = var.timeouts,
-  "QueryStoreRuntimeStatistics" = var.query_store_runtime_statistics
-  "QueryStoreWaitStatistics"    = var.query_store_wait_statistics
-  "SQLinsights"                 = var.sql_insights
+module "server" {
+  count  = local.replica_enabled ? 2 : 1
+  source = "./modules/server"
+
+  suffix              = count.index + 1
+  location            = count.index == 0 ? var.location : local.secondary_region
+  resource_group_name = var.resource_group_name
+  names               = var.names
+  tags                = var.tags
+
+  administrator_login = var.administrator_login
+  ad_admin_login_name = var.ad_admin_login_name
+
+  enable_extended_auditing_policy       = var.audit_log_enabled && count.index == 0
+  enable_threat_detection_policy        = var.enable_threat_detection_policy && count.index == 0
+  enable_active_directory_administrator = var.enable_active_directory_administrator
+
+  log_retention_days = var.log_retention_days
+  service_endpoints  = count.index == 0 ? var.service_endpoints : {}
+  access_list        = count.index == 0 ? var.access_list : {}
+
+  storage = {
+    endpoint     = azurerm_storage_account.sa["${count.index == 0 ? "primary" : "secondary"}_blob_endpoint"]
+    access_key   = azurerm_storage_account.sa["${count.index == 0 ? "primary" : "secondary"}_access_key"]
+    is_secondary = count.index == 1
   }
 }
 
+# SQL Server Database within a SQL Server Server
+module "db" {
+  for_each           = local.databases
+  source             = "./modules/db"
+  name               = each.key
+  server_id          = module.server[0].id
+  collation          = each.value.collation
+  license_type       = each.value.license_type
+  sku_name           = each.value.sku_name
+  max_size_gb        = each.value.max_size_gb
+  zone_redundant     = each.value.zone_redundant
+  read_scale         = each.value.read_scale
+  read_replica_count = each.value.read_replica_count
 
-# resource "azurerm_mssql_database_extended_auditing_policy" "db" {
-#   count                      = var.audit_log_enabled ? 1 : 0
-#   database_id                = azurerm_mssql_database.db.id
-#   storage_endpoint           = var.storage.endpoint
-#   storage_account_access_key = var.storage.access_key
-#   retention_in_days          = var.log_retention_days
-# }
+  audit_log_enabled = var.audit_log_enabled
+
+  log_retention_days = var.log_retention_days
+  storage            = { endpoint = azurerm_storage_account.sa.primary_blob_endpoint, access_key = azurerm_storage_account.sa.primary_access_key }
+}
+
+# Azure SQL Failover Group - Default is "false" 
+resource "azurerm_sql_failover_group" "fog" {
+  count               = local.replica_enabled ? 1 : 0
+  name                = "${var.names.product_name}-${var.names.environment}-failover-group"
+  resource_group_name = var.resource_group_name
+  server_name         = module.server[0].name
+  databases           = [for db in values(module.db) : db.id]
+  tags                = var.tags
+  partner_servers {
+    id = module.server[1].id
+  }
+
+  read_write_endpoint_failover_policy {
+    mode          = "Automatic"
+    grace_minutes = 60
+  }
+
+  readonly_endpoint_failover_policy {
+    mode = "Enabled"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,28 +1,66 @@
-#######
-# Name 
-#######
-variable "name" {
-  description = "The name of the Ms SQL Database. Changing this forces a new resource to be created."
+#
+# Required parameters
+##
+
+variable "location" {
+  description = "Specifies the supported Azure location to SQL Server server resource"
   type        = string
 }
 
-############
-# Server Id 
-############
-variable "server_id" {
-  description = "The id of the Ms SQL Server on which to create the database. Changing this forces a new resource to be created."
+variable "resource_group_name" {
+  description = "name of the resource group to create the resource"
   type        = string
 }
 
-#######
-# Flags
-#######
+variable "names" {
+  description = "names to be applied to resources"
+  type        = map(string)
+}
 
-# Audit Log Enabled 
-variable "audit_log_enabled" {
-  description = "The audit log enabled of the resource."
-  type        = bool
-  default     = false
+variable "tags" {
+  description = "tags to be applied to resources"
+  type        = map(string)
+}
+
+variable "sku_name" {
+  description = "Azure database for SQL Server sku name"
+  type        = string
+  default     = "GP_Gen5_2"
+}
+
+variable "storage_mb" {
+  description = "Max storage allowed for a server"
+  type        = number
+  default     = "10240"
+}
+
+variable "sql_version" {
+  description = "SQL Server version"
+  type        = string
+  default     = "12.0"
+}
+
+variable "administrator_login" {
+  type        = string
+  description = "Database administrator login name"
+  default     = "az_dbadmin"
+}
+
+variable "create_mode" {
+  description = "Can be used to restore or replicate existing servers. Possible values are Default and Replica. Defaults to Default"
+  type        = string
+  default     = "Default"
+
+  validation {
+    condition     = var.create_mode == "Default" || var.create_mode == "Replica"
+    error_message = "Create mode must be set to \"Default\" or \"Replica\"."
+  }
+}
+
+variable "creation_source_server_name" {
+  description = "the source server name to use. use this only when creating a read replica server"
+  type        = string
+  default     = ""
 }
 
 variable "log_retention_days" {
@@ -31,120 +69,201 @@ variable "log_retention_days" {
   default     = 7
 }
 
-
-##########
-# Storage 
-##########
-variable "storage" {
-  description = "Specifies the identifier key of the Threat Detection audit storage account. Required if retention is Enabled."
-  type        = object({ endpoint = string, access_key = string })
-
-  default = { "endpoint" = "", "access_key" = "" }
+variable "ssl_enforcement_enabled" {
+  description = "Specifies if SSL should be enforced on connections. Possible values are true and false."
+  type        = bool
+  default     = true
 }
 
-variable "collation" {
-  description = "Specifies the collation of the database. Changing this forces a new resource to be created."
-  type        = string
-  default     = "SQL_Latin1_General_CP1_CI_AS"
+variable "infrastructure_encryption_enabled" {
+  description = "Whether or not infrastructure is encrypted for this server. Defaults to false. Changing this forces a new resource to be created."
+  type        = bool
+  default     = false
 }
-variable "license_type" {
-  description = "Specifies the license type applied to this database. Possible values are LicenseIncluded and BasePrice."
-  type        = string
-  default     = "LicenseIncluded"
-}
-variable "sku_name" {
-  description = "Specifies the name of the sku used by the database. Changing this forces a new resource to be created. For example, GP_S_Gen5_2,HS_Gen4_1,BC_Gen5_2, ElasticPool, Basic,S0, P2 ,DW100c, DS100."
-  type        = string
-  default     = "GP_S_Gen5_1"
 
-  validation {
-    condition     = can(regex("GP_S_Gen5_1|GP_S_Gen5_2|HS_Gen4_1|BC_Gen5_2|ElasticPool|Basic|S0|P2|DW100c|DS100", var.sku_name))
-    error_message = "Sku name invalid. Must be GP_S_Gen5_1,GP_S_Gen5_2,HS_Gen4_1,BC_Gen5_2, ElasticPool, Basic,S0, P2 ,DW100c, DS100."
+variable "auto_grow_enabled" {
+  description = "Enable/Disable auto-growing of the storage."
+  type        = bool
+  default     = false
+}
+
+variable "service_endpoints" {
+  description = "Creates a virtual network rule in the subnet_id (values are virtual network subnet ids)."
+  type        = map(string)
+  default     = {}
+}
+
+variable "access_list" {
+  description = "Access list for SQL Server instance. Map off names to cidr ip start/end addresses"
+  type = map(object({ start_ip_address = string,
+  end_ip_address = string }))
+  default = {}
+}
+
+variable "enable_active_directory_administrator" {
+  description = "Set a user or group as the AD administrator for an SQL Server server in Azure"
+  type        = bool
+  default     = false
+}
+
+variable "ad_admin_login_name" {
+  description = "The login name of the azure ad admin."
+  type        = string
+  default     = ""
+}
+
+##
+# Optional Parameters
+##
+
+variable "backup_retention_days" {
+  description = "Backup retention days for the server, supported values are between 7 and 35 days."
+  type        = number
+  default     = 7
+}
+
+variable "geo_redundant_backup_enabled" {
+  description = "Turn Geo-redundant server backups on/off. This allows you to choose between locally redundant or geo-redundant backup storage in the General Purpose and Memory Optimized tiers. When the backups are stored in geo-redundant backup storage, they are not only stored within the region in which your server is hosted, but are also replicated to a paired data center. This provides better protection and ability to restore your server in a different region in the event of a disaster. This is not supported for the Basic tier."
+  type        = bool
+  default     = false
+}
+
+variable "enable_threat_detection_policy" {
+  description = "Threat detection policy configuration, known in the API as Server Security Alerts Policy."
+  type        = bool
+  default     = false
+}
+
+##
+# Required SQL Server Server Parameters
+##
+
+variable "audit_log_enabled" {
+  description = "Whether to enable auditing of logs."
+  type        = bool
+  default     = true
+}
+
+variable "max_allowed_packet" {
+  type        = string
+  description = "The maximum size of one packet or any generated/intermediate string, or any parameter sent by the sql_stmt_send_long_data() C API function."
+  default     = "1073741824"
+}
+
+variable "max_connections" {
+  type        = number
+  description = "The maximum permitted number of simultaneous client connections. value 10-600"
+  default     = 600
+}
+
+variable "max_heap_table_size" {
+  type        = string
+  description = "This variable sets the maximum size to which user-created MEMORY tables are permitted to grow."
+  default     = "64000000"
+}
+
+variable "performance_schema" {
+  type        = string
+  description = "The value of this variable is ON or OFF to indicate whether the Performance Schema is enabled."
+  default     = "ON"
+}
+
+variable "replicate_wild_ignore_table" {
+  type        = string
+  description = "Creates a replication filter which keeps the slave thread from replicating a statement in which any table matches the given wildcard pattern. To specify more than one table to ignore, use comma-separated list."
+  default     = "sql.%,tempdb.%"
+}
+
+variable "slow_query_log" {
+  type        = string
+  description = "Enable or disable the slow query log"
+  default     = "OFF"
+}
+
+variable "sort_buffer_size" {
+  type        = number
+  description = "Each session that must perform a sort allocates a buffer of this size."
+  default     = 2000000
+}
+
+variable "tmp_table_size" {
+  type        = number
+  description = "The maximum size of internal in-memory temporary tables. This variable does not apply to user-created MEMORY tables."
+  default     = 64000000
+}
+
+variable "transaction_isolation" {
+  type        = string
+  description = "The default transaction isolation level."
+  default     = "READ-COMMITTED"
+}
+
+variable "query_store_capture_interval" {
+  type        = string
+  description = "The query store capture interval in minutes. Allows to specify the interval in which the query metrics are aggregated."
+  default     = "15"
+}
+
+variable "query_store_capture_mode" {
+  type        = string
+  description = "The query store capture mode, NONE means do not capture any statements. NOTE: If performance_schema is OFF, turning on query_store_capture_mode will turn on performance_schema and a subset of performance schema instruments required for this feature."
+  default     = "ALL"
+}
+
+variable "query_store_capture_utility_queries" {
+  type        = string
+  description = "Turning ON or OFF to capture all the utility queries that is executing in the system."
+  default     = "YES"
+}
+
+variable "query_store_retention_period_in_days" {
+  type        = number
+  description = "The query store capture interval in minutes. Allows to specify the interval in which the query metrics are aggregated."
+  default     = 7
+}
+
+variable "query_store_wait_sampling_capture_mode" {
+  type        = string
+  description = "The query store wait event sampling capture mode, NONE means do not capture any wait events."
+  default     = "ALL"
+}
+
+variable "query_store_wait_sampling_frequency" {
+  type        = number
+  description = "The query store wait event sampling frequency in seconds."
+  default     = 30
+}
+
+variable "databases" {
+  description = "Map of databases to create (keys are database names). Allowed values are the same as for database_defaults."
+  type        = map(any)
+  default     = {}
+}
+
+variable "database_defaults" {
+  description = "database default charset and collation (only applied to databases managed within this module)"
+  type = object({
+    collation          = string
+    license_type       = string
+    sku_name           = string
+    max_size_gb        = number
+    zone_redundant     = bool
+    read_scale         = bool
+    read_replica_count = number
+  })
+  default = {
+    collation                   = "SQL_Latin1_General_CP1_CI_AS"
+    license_type                = "LicenseIncluded"
+    sku_name                    = "GP_S_Gen5_1"
+    min_capacity                = 0.5
+    auto_pause_delay_in_minutes = -1
+    max_size_gb                 = 4
+    zone_redundant              = false
+    read_scale                  = false
+    read_replica_count          = 0
   }
 }
-variable "min_capacity" {
-  type    = number
-  default = 0.5
-}
-variable "auto_pause_delay_in_minutes" {
-  type    = number
-  default = -1
-}
-variable "max_size_gb" {
-  type    = number
-  default = 4
-}
-variable "zone_redundant" {
-  type    = bool
-  default = false
-}
-variable "read_scale" {
-  type    = bool
-  default = false
-}
-variable "read_replica_count" {
-  type    = number
-  default = 0
-}
 
-##
-# Diagnostic setting variable
-##
-
-variable "storage_endpoint" {
-    description = "This blob storage will hold all Threat Detection audit logs. Required if state is Enabled."
-    type        = string
-    default     = ""
+locals {
+  databases = zipmap(keys(var.databases), [for database in values(var.databases) : merge(var.database_defaults, database)])
 }
-
-variable "storage_account_resource_group" {
-  description = "Azure resource group where the storage account resides."
-  type        = string
-}
-
-variable "automatic_tuning" {
-  description = "Retention only applies to storage account. Retention policy ranges from 1 to 365 days. If you do not want to apply any retention policy and retain data forever, set retention (days) to 0."
-  type        = number
-}
-
-variable "database_wait_statistics" {
-  description = "Retention only applies to storage account. Retention policy ranges from 1 to 365 days. If you do not want to apply any retention policy and retain data forever, set retention (days) to 0."
-  type        = number
-}
-
-variable "query_store_runtime_statistics" {
-  description = "Retention only applies to storage account. Retention policy ranges from 1 to 365 days. If you do not want to apply any retention policy and retain data forever, set retention (days) to 0."
-  type        = number
-}
-
-variable "query_store_wait_statistics" {
-  description = "Retention only applies to storage account. Retention policy ranges from 1 to 365 days. If you do not want to apply any retention policy and retain data forever, set retention (days) to 0."
-  type        = number
-}
-
-variable "error_log" {
-  description = "Retention only applies to storage account. Retention policy ranges from 1 to 365 days. If you do not want to apply any retention policy and retain data forever, set retention (days) to 0."
-  type        = number
-}
-
-variable "sql_insights" {
-  description = "Retention only applies to storage account. Retention policy ranges from 1 to 365 days. If you do not want to apply any retention policy and retain data forever, set retention (days) to 0."
-  type        = number
-}
-
-variable "deadlocks" {
-  description = "Retention only applies to storage account. Retention policy ranges from 1 to 365 days. If you do not want to apply any retention policy and retain data forever, set retention (days) to 0."
-  type        = number
-}
-
-variable "timeouts" {
-  description = "Retention only applies to storage account. Retention policy ranges from 1 to 365 days. If you do not want to apply any retention policy and retain data forever, set retention (days) to 0."
-  type        = number
-}
-
-
-variable "metric" {
-  description = "Retention only applies to storage account. Retention policy ranges from 1 to 365 days. If you do not want to apply any retention policy and retain data forever, set retention (days) to 0."
-  type        = number
-}
-
